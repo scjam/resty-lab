ed13e554f8c76da6a5c8a906793840abdf9c2bbb
Sydney James <sydneyjames7@gmail.com>
2021-02-04 15:51:54 -0800

    added header presentational component and Resty container

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Resty from '../containers/Resty';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Resty />
+  );
 }
---
+++ src/containers/Resty.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from '../components/presentational/Header';
+
+export default class Resty extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}

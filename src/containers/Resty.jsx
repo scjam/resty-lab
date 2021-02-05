7e49c0eb269930051f6806a22703ad524cfe7a98
Sydney James <sydneyjames7@gmail.com>
2021-02-04 17:01:42 -0800

    state and handler for onChange done

--- src/containers/Resty.jsx
+++ src/containers/Resty.jsx
@@ -3,11 +3,33 @@ import Form from '../components/presentational/Form';
 import Header from '../components/presentational/Header';
 
 export default class Resty extends Component {
+  state={
+    url: '',
+    method: '',
+    json: '',
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  // handleSubmit = ({ target }) => {
+
+  // }
+
   render() {
+    const { url, method, json } = this.state;
+
     return (
       <>
         <Header />
-        <Form />
+        <Form 
+          url={url}
+          method={method}
+          json={json}
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+        />
       </>
     );
   }
